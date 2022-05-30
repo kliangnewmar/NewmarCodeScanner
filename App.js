4ed2876af30657acac2d749863973c3b5ab45972
kliangnewmar <kliang@newmar.com>
2022-05-30 13:36:27 -0400

    Updated SDK to 45

--- App.js
+++ App.js
@@ -1,41 +1,35 @@
-import { StatusBar } from "expo-status-bar";
 import Scanner from "./Scanner";
 import Home from "./Home";
 import React from "react";
 import { createAppContainer } from "react-navigation";
-import { StyleSheet, Text, View, Button, Alert } from "react-native";
-import { createStackNavigator, HeaderTitle, HeaderBackground } from "react-navigation-stack";
+import { createStackNavigator } from "react-navigation-stack";
 
 const AppNavigator = createStackNavigator(
   {
-    Home:{
-      
-      screen:Home,
+    Home: {
+      screen: Home,
       navigationOptions: {
-        headerTitle: 'Newmar Code Scanner',
-        headerTintColor:'white',
-        headerStyle:{
-          backgroundColor:'#4169e1',
-        }
+        headerTitle: "Newmar Code Scanner",
+        headerTintColor: "white",
+        headerStyle: {
+          backgroundColor: "#4169e1",
+        },
       },
-      
     },
-    Scanner:{
-      screen:Scanner,
+    Scanner: {
+      screen: Scanner,
       navigationOptions: {
-        headerTitle: 'QRCode Scanner',
-        headerTintColor:'white',
-        headerStyle:{
-          backgroundColor:'#4169e1'
-        }
+        headerTitle: "QRCode Scanner",
+        headerTintColor: "white",
+        headerStyle: {
+          backgroundColor: "#4169e1",
+        },
       },
-    }
-    
+    },
   },
   {
     initialRouteName: "Home",
-  },
- 
+  }
 );
 
 const AppContainer = createAppContainer(AppNavigator);
